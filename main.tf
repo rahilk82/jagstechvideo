60a881ff88aaa03eed83849cc89b5ce0b0ead5d0
jagasworld <jagasworld@gmail.com>
2020-07-02 23:33:17 +0530

    vpctest

---
+++ main.tf
@@ -0,0 +1,12 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_vpc" "production-vpc" {
+  cidr_block            = "${var.vpc_cidr}"
+  enable_dns_hostnames  = true
+
+  tags {
+    Name = "Production-VPC"
+  }
+}
